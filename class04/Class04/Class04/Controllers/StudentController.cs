fe2d065383674a136b9790cc6dcbe4e9b50fcba5
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-06-19 21:43:50 +0200

    cl04

---
+++ class04/Class04/Class04/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using Class04.Models.DtoModels;
+using Class04.Models.ViewModels;
+using Class04.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Class04.Controllers
+{
+    //[Route("students")]
+    [Route("[Controller]")]
+    public class StudentController : Controller
+    {
+        private readonly StudentService _studentService;
+
+        public StudentController()
+        {
+            _studentService = new StudentService();
+        }
+
+        //[HttpGet("getAllStudents")]
+        public IActionResult GetAllStudents()
+        {
+            List<StudentWithCourseDto> students = _studentService.GetAllStudents();
+
+            return View(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            StudentWithCourseDto student = _studentService.GetStudentById(id);
+
+            return View(student);
+        }
+        [HttpGet("createStudent")]
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+        [HttpPost("createStudent")]
+        public IActionResult CreateStudent(CreateStudentVM createStudentVM)
+        {
+            _studentService.CreateStudent(createStudentVM);
+            return RedirectToAction("GetAllStudents");
+        }
+
+    }
+}
+
---
+++ class04/Class04/Class04/Services/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using Class04.DataBase;
+using Class04.Models.DtoModels;
+using Class04.Models.Entities;
+using Class04.Models.ViewModels;
+
+namespace Class04.Services
+{
+    public class StudentService
+    {
+        public List<StudentWithCourseDto> GetAllStudents()
+        {
+            List<StudentWithCourseDto> students = InMemoryDb.Students.Select(x => new StudentWithCourseDto(x.Id, x.FirstName, x.LastName, x.DateOfBirth, x.ActiveCourse.Name)).ToList();
+
+            return students;
+            //List<StudentWithCourseDto> students = InMemoryDb.Students.Select(x => new StudentWithCourseDto
+            //{
+            //    Id = x.Id,
+            //    FullName = $"{x.FirstName} {x.LastName}"
+            //}().ToList();
+        }
+
+        public StudentWithCourseDto GetStudentById(int id)
+        {
+            Student student = InMemoryDb.Students.SingleOrDefault(x => x.Id == id);
+
+            if (student == null)
+            {
+                return null;
+            }
+
+            return new StudentWithCourseDto(student.Id, student.FirstName, student.LastName, student.DateOfBirth, student.ActiveCourse.Name);
+
+        }
+        public void CreateStudent(CreateStudentVM viewModel)
+        {
+            Student entity = new Student
+            {
+                Id = InMemoryDb.Students.Count + 1,
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                DateOfBirth = viewModel.DateOfBirth,
+                ActiveCourse = InMemoryDb.Courses[2]
+            };
+
+            InMemoryDb.Students.Add(entity);
+        }
+    }
+}
