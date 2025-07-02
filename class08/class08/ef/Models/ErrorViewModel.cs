be6845d27a47fef1c6f15724e036e555ed939505
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-07-02 07:29:28 +0200

    sadsa

--- class08/class08/ef/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ef.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
