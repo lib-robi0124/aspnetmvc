2175fc0c99fadcbe74bbd00653fb1a2777437d96
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-07-21 08:32:10 +0200

    vde

---
+++ MovieRental/MovieRentalApp/MovieRentalApp.Dtos/FilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieRentalApp.Dtos
+{
+    public class FilterDto
+    {
+        public List<MovieDto> Movies { get; set; }
+        public List<RentalDto> Rentals { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
